3a4e1bb686412742a4f4c952ccb12ea517bf657e
lifeinpassion <lifeinpassion@gmail.com>
2023-01-18 19:47:24 +0200

    initial modifications

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -71,7 +71,18 @@ public_users.get('/books/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
-  res.send(books[author]);
+  // iterate through the value of the books object and find all the books by the author
+  let books_by_author = [];
+  for (let book in books) {
+    if (books[book].author === author) {
+      books_by_author.push(books[book]);
+    }
+  }
+  if (books_by_author.length === 0) {
+    res.send("No books by this author");
+  } else {
+    res.send(books_by_author);
+  }
 });
 
 // Task 12 - Get book details based on author using Promise callbacks or async-await with Axios
@@ -87,7 +98,18 @@ public_users.get('/books/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
-  res.send(books[title]);
+  // iterate through the value of the books object and find all the book with the specific title
+  let books_by_title = [];
+  for (let book in books) {
+    if (books[book].title === title) {
+      books_by_title.push(books[book]);
+    }
+  }
+  if (books_by_title.length === 0) {
+    res.send("No books with this title");
+  } else {
+    res.send(books_by_title);
+  }
 });
 
 // Task 13 - Get book details based on title using Promise callbacks or async-await with Axios
@@ -103,7 +125,7 @@ public_users.get('/books/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
   //Get the book reviews based on ISBN provided in the request parameters.
   const isbn = req.params.isbn;
-  res.send(reviews[isbn]);
+  res.send(books[isbn]["reviews"]);
 });
 
 module.exports.general = public_users;
